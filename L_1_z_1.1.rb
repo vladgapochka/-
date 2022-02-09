e2d7d93d7d25817ce1004b191adce27f3ae958ac
Vlad <vlad.gapochk@mail.ru>
2022-02-09 19:13:46 +0300

    Лаб_1_задание_1.2

--- L_1_z_1.1.rb
+++ L_1_z_1.1.rb
@@ -1,5 +1,10 @@
-puts "hello world" # Комментарий
+# puts "hello world" # Комментарий
 =begin
 многострочный коментарий
 =end
+
+# выводим приветствие
+puts "Приветствую #{ARGV[0]}"
+# получаем методы класса String
+puts String.instance_methods
 
