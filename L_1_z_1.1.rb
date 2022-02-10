0fe1ee8f8e849d5321af6dc881d41609db2b85fd
Vlad <vlad.gapochk@mail.ru>
2022-02-10 11:44:16 +0300

    Лаб_1_задание_1.4

--- L_1_z_1.1.rb
+++ L_1_z_1.1.rb
@@ -7,12 +7,18 @@
 # puts "Приветствую #{ARGV[0]}"
 # puts String.instance_methods
 # Задание 1.3
-puts "Введите свой любимый язык программировани:"
-l = gets.chomp
-puts("подлиза") if l == "ruby"
-puts("скоро будет ruby, а так ты молодец") if l == "питон"
-puts("скоро будет ruby,но с++ фу") if l == "с++"
-puts("скоро будет ruby, но ты слабый") if l == "с#"
-puts("скоро будет ruby, ты вообще больной?") if l == "паскаль"
+# puts "Введите свой любимый язык программировани:"
+# l = gets.chomp
+# puts("подлиза") if l == "ruby"
+# puts("скоро будет ruby, а так ты молодец") if l == "питон"
+# puts("скоро будет ruby,но с++ фу") if l == "с++"
+# puts("скоро будет ruby, но ты слабый") if l == "с#"
+# puts("скоро будет ruby, ты вообще больной?") if l == "паскаль"
 
-
+#Задание 1.4
+puts "Введите команду Ruby"
+rub=STDIN.gets
+eval(rub)
+puts "Введите команду OC"
+rub=STDIN.gets
+system(rub)
