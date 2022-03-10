c9b265eba85a81447ab4c1e0e21710bed4a52e23
Влад <gapochka2000@mail.tu>
2022-03-10 18:50:37 +0300

    Лаба_1_задание_3.2

--- L_1_z_22.rb
+++ L_1_z_22.rb
@@ -10,45 +10,57 @@
 x=ARGV[0].to_i
 
 
-def method_sum(x)
-	sum = 0
-	while x != 0
-		sum += x%10
-		x=x/10
-	end
-	puts sum
-end
+# def method_sum(x)
+# 	sum = 0
+# 	while x != 0
+# 		sum += x%10
+# 		x=x/10
+# 	end
+# 	puts sum
+# end
 
-def method_min(x)
-	min = 100000
-	while x!=0
-		if x%10<min
-			min = x%10			
-		end
-		x = x/10
+# def method_min(x)
+# 	min = 100000
+# 	while x!=0
+# 		if x%10<min
+# 			min = x%10			
+# 		end
+# 		x = x/10
 
-	end
-	puts min
-end
+# 	end
+# 	puts min
+# end
+# def method_max(x)
+# 	max = 0
+# 	while x!=0
+# 		if x%10>max
+# 			max = x%10
+# 		end
+# 		x = x/10
+# 	end
+# 	puts max
+# end
+# def method_proiz(x)
+# 	pr = 1
+# 	while x!=0
+# 		pr *= x%10
+# 		x = x/10
+# 	end
+# 	puts pr
+# end
+# puts method_sum(x)
+# puts method_min(x)
+# puts method_max(x)
+# puts method_proiz(x)
+# Задание 3.2
 def method_max(x)
-	max = 0
-	while x!=0
-		if x%10>max
-			max = x%10
+	max=0
+	while x !=0
+		if (x%10)%3!=0 and x %10>max
+			max=x%10
 		end
-		x = x/10
+		x=x/10
 	end
 	puts max
 end
-def method_proiz(x)
-	pr = 1
-	while x!=0
-		pr *= x%10
-		x = x/10
-	end
-	puts pr
-end
-puts method_sum(x)
-puts method_min(x)
-puts method_max(x)
-puts method_proiz(x)
+method_max(x)
